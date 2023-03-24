f29877f0ee2cff94a56e0c92088b42b8fcf62a5c
Bruno <bruno_moises_stz@hotmail.com>
2023-03-24 10:34:49 -0300

    style: criando o tema com as cores do projeto

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,11 @@
+export const theme = {
+    title: 'theme',
+    colors: {
+        primary: '#1B2033',
+        secondary: '#1C2C44',
+        tertiary: '#115678',
+        blue1: '#036FC0',
+        blue2: '#00B6DE',
+        white: '#FDFDFD',
+    }
+}
