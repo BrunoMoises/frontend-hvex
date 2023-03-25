bb930b97c5420efb26a73f2c150590f91d653618
Bruno <bruno_moises_stz@hotmail.com>
2023-03-25 14:37:43 -0300

    style: removendo estilos desnecessários

--- src/components/AppTemplate/styles.ts
+++ src/components/AppTemplate/styles.ts
@@ -6,19 +6,3 @@ export const Container = styled.div`
     flex-direction: column;
     justify-content: space-between;
 `
-
-export const Header = styled.div`
-    height: 70px;
-    width: 100vw;
-    background-color: ${(props) => props.theme.colors.header};
-    position: fixed;
-    top: 0;
-`
-
-export const Footer = styled.div`
-    height: 225px;
-    width: 100vw;
-    background-color: ${(props) => props.theme.colors.secondary};
-    position: fixed;
-    bottom: 0;
-`
