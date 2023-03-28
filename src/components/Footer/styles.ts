b7b06117237cc0a9f1e6949834502572caa5fbca
Bruno <bruno_moises_stz@hotmail.com>
2023-03-28 15:19:48 -0300

    style: alterando detalhes de tamanho e position na footer

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -4,10 +4,10 @@ export const Container = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    height: 180px;
-    width: 100vw;
+    height: 323px;
+    width: 100%;
     background-color: ${(props) => props.theme.colors.secondary};
-    position: fixed;
+    position: relative;
     bottom: 0;
 
     a {
@@ -23,8 +23,8 @@ export const Content = styled.div`
     display: flex;
     justify-content: right;
     align-items: center;
-    right: 70px;
-    gap: 30px;
+    right: 10%;
+    gap: 10px;
 `
 
 export const Box = styled.div`
@@ -32,28 +32,27 @@ export const Box = styled.div`
     justify-content: center;
     align-items: flex-start;
     flex-direction: column;
-    width: 200px;
-    height: 180px;
-    gap: 10px;
+    width: 275px;
 
     h1 {
-        bottom: 130px;
-        position: fixed;
+        position: absolute;
+        bottom: 70px;
         font-size: 20px;
         color: ${(props) => props.theme.colors.blue2};
     }
 
     p {
-        position: relative;
+        position: absolute;
+        width: 230px;
         font-size: 16px;
         color: ${(props) => props.theme.colors.white};
     }
 `
 
 export const BoxIcons = styled.div`
-    right: 80px;
+    right: 8%;
     gap: 20px;
-    bottom: 110px;
+    bottom: 30px;
     position: absolute;
     display: flex;
     justify-content: center;
@@ -61,5 +60,20 @@ export const BoxIcons = styled.div`
 
     a {
         position: relative;
+    }
+`
+
+export const FooterCopyRight = styled.div`
+    display: flex;
+    position: absolute;
+    justify-content: space-between;
+    align-items: center;
+    width: 90%;
+    color: ${(props) => props.theme.colors.white};
+    border-top: 1px solid ${(props) => props.theme.colors.white};
+    bottom: 20px;
+
+    span {
+        margin-top: 20px;
     }
 `
