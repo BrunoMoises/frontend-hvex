6729561fd4d8a69e62e61c9eed05067bbcf38754
Bruno <bruno_moises_stz@hotmail.com>
2023-03-28 15:20:32 -0300

    feat: adicionando sequencia de paginas na home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,22 @@
+import { Container } from "../styles/styles";
+import { Page1 } from "@/components/Page1";
+import { Page2 } from "@/components/Page2";
+import { Page3 } from "@/components/Page3";
+import { Page4 } from "@/components/Page4";
+import { Page5 } from "@/components/Page5";
+import { Page6 } from "@/components/Page6";
+import { Page7 } from "@/components/Page7";
+
 const Home = () => (
-  <>
-  <h1>Hello</h1>
-  </>
+  <Container>
+    <Page1 />
+    <Page2 />
+    <Page3 />
+    <Page4 />
+    <Page5 />
+    <Page6 />
+    <Page7 />
+  </Container>
 );
 
 export default Home;
