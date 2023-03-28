4a1bb9484a5f87f3a951018edc6fcb2e839c97c2
Bruno <bruno_moises_stz@hotmail.com>
2023-03-28 08:46:30 -0300

    style: adicionando fonte inter no projeto

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,18 @@
 import { AppTemplate } from '@/components/AppTemplate'
 import { theme } from '@/styles/themes/dark'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyles from '../styles/global'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet" />
+      </Head>
       <GlobalStyles />
       <AppTemplate>
         <Component {...pageProps} />
