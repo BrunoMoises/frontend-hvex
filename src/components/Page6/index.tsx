1fe77c3bde77c3e5630ac925e28659416fed3fba
Bruno <bruno_moises_stz@hotmail.com>
2023-03-28 15:17:09 -0300

    feat: criando conteudo da pagina 6

---
+++ src/components/Page6/index.tsx
@@ -0,0 +1,11 @@
+import { Button } from "../Button"
+import { Container, Titulo } from "./styles"
+
+export const Page6 = () => {
+    return (
+        <Container>
+            <Titulo>Quer saber mais sobre como podemos te ajudar a melhorar a performance de seus ativos?</Titulo>
+            <Button>Falar com um consultor</Button>
+        </Container>
+    )
+}
