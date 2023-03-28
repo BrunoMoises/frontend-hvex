283bce2a409d48d9b306ae165ad2aed69264634e
Bruno <bruno_moises_stz@hotmail.com>
2023-03-28 16:35:54 -0300

    style: estilização do componente de card

---
+++ src/components/Card/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const CardBody = styled.div`
+    background: #F9F9F9;
+    border: 1px solid rgba(0, 0, 0, 0.1);
+    box-shadow: 0px 10px 20px -5px rgba(0, 0, 0, 0.19);
+    border-radius: 10px;
+    width: 770px;
+`
