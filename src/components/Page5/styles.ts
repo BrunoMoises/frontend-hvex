e105085e7fbfe64209dc028c8ed09920818d371c
Bruno <bruno_moises_stz@hotmail.com>
2023-03-28 15:17:00 -0300

    style: criando estilos da pagina 5 e adicionando png do icone utilizado

---
+++ src/components/Page5/styles.ts
@@ -0,0 +1,62 @@
+import styled from 'styled-components'
+import bg from '../../assets/background4.png'
+
+export const Container = styled.div`
+    position: relative;
+    width: 100%;
+    height: 750px;
+    left: 0px;
+    background-color: #F4F4F4;
+`
+export const Img = styled.div`
+    position: absolute;
+    width: 800px;
+    height: 879px;
+    left: 47%;
+    top: 5%;
+
+    background: url(${bg.src});
+    background-repeat: no-repeat;
+`
+
+export const Content = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+    height: 100%;
+    position: absolute;
+    top: 15%;
+    left: 10%;
+    gap: 50px;
+`
+
+export const Titulo = styled.div`
+    width: 50%;
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 32px;
+    line-height: 40px;
+    color: ${(props) => props.theme.colors.secondary};
+    padding-bottom: 27px;
+    border-bottom: 5px solid ${(props) => props.theme.colors.blue2};
+`
+
+export const List = styled.ul`
+    list-style: none;
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 20px;
+    line-height: 155%;
+
+    li {
+        color: '#373737';
+        margin-bottom: 30px;
+    }
+
+    span {
+        margin-left: 30px;
+    }
+`
